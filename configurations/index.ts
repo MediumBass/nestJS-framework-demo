f0ba573b29e99026a7f11e44dc2f2ed2a348ec7f
mediumbass <newfallout.410@gmail.com>
2024-08-04 03:26:19 +0300

    initial

---
+++ configurations/index.ts
@@ -0,0 +1,10 @@
+import {env} from "process";
+
+export  default () => ({
+    port: env.PORT,
+    db_port: env.DB_PORT,
+    db_name: env.DB_NAME,
+    db_user: env.DB_USER,
+    db_password: env.DB_PASSWORD,
+    db_host: env.DB_HOST
+})
---
+++ src/moks/index.ts
@@ -0,0 +1,319 @@
+export const users=[
+    [
+        {
+            "_id": "66a415432e27cd3f86d18865",
+            "index": 0,
+            "guid": "e8c62e20-3bc3-4e6a-b6c3-08a3c8750c04",
+            "isActive": true,
+            "balance": "$1,388.39",
+            "picture": "http://placehold.it/32x32",
+            "age": 34,
+            "eyeColor": "green",
+            "name": "Tonia Collier",
+            "gender": "female",
+            "company": "OMATOM",
+            "email": "toniacollier@omatom.com",
+            "phone": "+1 (983) 439-2035",
+            "address": "277 Alabama Avenue, Chautauqua, New York, 9446",
+            "about": "Voluptate duis occaecat commodo laborum pariatur cupidatat sit excepteur non minim. Incididunt dolore cillum cillum enim dolore id veniam. Labore adipisicing do officia aliquip mollit id ea excepteur cillum voluptate. Est anim adipisicing ut nulla anim in amet. Quis eiusmod est laboris ut magna elit aliquip ex voluptate cillum adipisicing sint voluptate excepteur.\r\n",
+            "registered": "2021-08-02T12:19:02 -03:00",
+            "latitude": 22.120614,
+            "longitude": -124.338368,
+            "tags": [
+                "reprehenderit",
+                "laboris",
+                "irure",
+                "consectetur",
+                "velit",
+                "reprehenderit",
+                "ipsum"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Christy House"
+                },
+                {
+                    "id": 1,
+                    "name": "Ashley Reyes"
+                },
+                {
+                    "id": 2,
+                    "name": "Perkins Jones"
+                }
+            ],
+            "greeting": "Hello, Tonia Collier! You have 1 unread messages.",
+            "favoriteFruit": "banana"
+        },
+        {
+            "_id": "66a415432efd8cbe008a8ca3",
+            "index": 1,
+            "guid": "d0c9df47-fccb-466e-b346-84b20f85117a",
+            "isActive": false,
+            "balance": "$1,423.96",
+            "picture": "http://placehold.it/32x32",
+            "age": 39,
+            "eyeColor": "blue",
+            "name": "Merle Montoya",
+            "gender": "female",
+            "company": "ENERVATE",
+            "email": "merlemontoya@enervate.com",
+            "phone": "+1 (974) 473-3261",
+            "address": "578 Fleet Place, Nipinnawasee, Tennessee, 4770",
+            "about": "Reprehenderit ea consectetur magna incididunt ut enim labore cillum mollit. Eiusmod fugiat aliquip et ullamco dolore qui et proident et. Ad est in sint magna duis sit reprehenderit esse pariatur.\r\n",
+            "registered": "2018-06-08T11:14:34 -03:00",
+            "latitude": -58.640229,
+            "longitude": 55.262369,
+            "tags": [
+                "irure",
+                "nulla",
+                "consequat",
+                "sit",
+                "consequat",
+                "commodo",
+                "ut"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Cecelia Floyd"
+                },
+                {
+                    "id": 1,
+                    "name": "Mcdonald Fry"
+                },
+                {
+                    "id": 2,
+                    "name": "Marian Patton"
+                }
+            ],
+            "greeting": "Hello, Merle Montoya! You have 10 unread messages.",
+            "favoriteFruit": "strawberry"
+        },
+        {
+            "_id": "66a41543ced6246665622724",
+            "index": 2,
+            "guid": "d0a4c7ca-bcce-4508-bef9-ec826e254014",
+            "isActive": true,
+            "balance": "$2,257.70",
+            "picture": "http://placehold.it/32x32",
+            "age": 40,
+            "eyeColor": "brown",
+            "name": "Charlene Coffey",
+            "gender": "female",
+            "company": "COMSTRUCT",
+            "email": "charlenecoffey@comstruct.com",
+            "phone": "+1 (985) 533-2034",
+            "address": "549 Sunnyside Avenue, Woodlands, Utah, 9042",
+            "about": "Elit officia exercitation ea aute. Proident nisi fugiat quis culpa tempor excepteur sunt proident pariatur cillum voluptate sunt occaecat. Non sint laboris veniam sunt laborum dolore. Ea aute consectetur ullamco duis elit tempor amet Lorem. Sit qui in proident est eiusmod. Deserunt consectetur officia cupidatat veniam excepteur. Eu aliquip eu nisi sunt veniam mollit esse cillum.\r\n",
+            "registered": "2017-08-03T03:35:28 -03:00",
+            "latitude": 83.577923,
+            "longitude": -1.369134,
+            "tags": [
+                "laboris",
+                "eiusmod",
+                "amet",
+                "adipisicing",
+                "ad",
+                "est",
+                "sit"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Bernard Dyer"
+                },
+                {
+                    "id": 1,
+                    "name": "Vincent Wallace"
+                },
+                {
+                    "id": 2,
+                    "name": "Luz Wyatt"
+                }
+            ],
+            "greeting": "Hello, Charlene Coffey! You have 3 unread messages.",
+            "favoriteFruit": "apple"
+        },
+        {
+            "_id": "66a41543d025c5acdde1e431",
+            "index": 3,
+            "guid": "cf1e0f29-bcb4-463d-bbf4-c1b9b66c2d07",
+            "isActive": true,
+            "balance": "$3,284.62",
+            "picture": "http://placehold.it/32x32",
+            "age": 31,
+            "eyeColor": "blue",
+            "name": "Bullock Graves",
+            "gender": "male",
+            "company": "AMTAP",
+            "email": "bullockgraves@amtap.com",
+            "phone": "+1 (865) 506-3626",
+            "address": "994 Gates Avenue, Lodoga, Alaska, 8946",
+            "about": "Cillum esse officia laboris mollit veniam sint esse mollit consequat reprehenderit labore consectetur. Et ad do nostrud ut duis. Enim amet excepteur consequat occaecat officia voluptate qui dolore culpa voluptate consectetur laboris. Exercitation fugiat aute amet consequat amet duis mollit occaecat. Culpa ad eiusmod qui et nostrud velit esse amet nulla eu occaecat labore in.\r\n",
+            "registered": "2024-02-14T12:39:14 -02:00",
+            "latitude": -81.571482,
+            "longitude": -29.078898,
+            "tags": [
+                "incididunt",
+                "ea",
+                "veniam",
+                "excepteur",
+                "deserunt",
+                "velit",
+                "Lorem"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Corina Villarreal"
+                },
+                {
+                    "id": 1,
+                    "name": "Luann Sampson"
+                },
+                {
+                    "id": 2,
+                    "name": "Cox Cook"
+                }
+            ],
+            "greeting": "Hello, Bullock Graves! You have 8 unread messages.",
+            "favoriteFruit": "banana"
+        },
+        {
+            "_id": "66a415438f9b5574f9e2daa4",
+            "index": 4,
+            "guid": "148fdf37-16e5-473f-aad6-127d0626c109",
+            "isActive": false,
+            "balance": "$3,395.90",
+            "picture": "http://placehold.it/32x32",
+            "age": 23,
+            "eyeColor": "blue",
+            "name": "Kelley Parrish",
+            "gender": "male",
+            "company": "DECRATEX",
+            "email": "kelleyparrish@decratex.com",
+            "phone": "+1 (800) 495-3419",
+            "address": "667 Virginia Place, Lafferty, Arizona, 8825",
+            "about": "Laborum aliquip occaecat ullamco consectetur elit eu excepteur laborum consectetur. Ad ut aute aliquip excepteur aliqua exercitation. Aliquip velit deserunt pariatur anim exercitation reprehenderit aliqua.\r\n",
+            "registered": "2017-08-18T01:06:36 -03:00",
+            "latitude": 2.008778,
+            "longitude": -134.053634,
+            "tags": [
+                "proident",
+                "dolor",
+                "aute",
+                "in",
+                "laborum",
+                "sunt",
+                "eiusmod"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Cohen Bird"
+                },
+                {
+                    "id": 1,
+                    "name": "Mamie Franco"
+                },
+                {
+                    "id": 2,
+                    "name": "Minerva Hampton"
+                }
+            ],
+            "greeting": "Hello, Kelley Parrish! You have 5 unread messages.",
+            "favoriteFruit": "strawberry"
+        },
+        {
+            "_id": "66a41543aee1fa2630d33c96",
+            "index": 5,
+            "guid": "8ca10230-03d2-47dd-b560-fd277430785b",
+            "isActive": true,
+            "balance": "$1,444.91",
+            "picture": "http://placehold.it/32x32",
+            "age": 30,
+            "eyeColor": "blue",
+            "name": "Ina England",
+            "gender": "female",
+            "company": "NEBULEAN",
+            "email": "inaengland@nebulean.com",
+            "phone": "+1 (884) 538-2191",
+            "address": "140 Desmond Court, Savannah, Idaho, 4757",
+            "about": "Fugiat sit adipisicing occaecat ipsum reprehenderit ex voluptate voluptate mollit. In culpa nisi proident elit dolore tempor commodo. Lorem sunt deserunt laboris anim occaecat ullamco est enim non. Ea esse pariatur velit sint qui est.\r\n",
+            "registered": "2021-01-22T07:04:40 -02:00",
+            "latitude": -15.514249,
+            "longitude": -171.241633,
+            "tags": [
+                "dolore",
+                "ad",
+                "ut",
+                "culpa",
+                "adipisicing",
+                "ad",
+                "minim"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Lynch Armstrong"
+                },
+                {
+                    "id": 1,
+                    "name": "Shields Sandoval"
+                },
+                {
+                    "id": 2,
+                    "name": "Donna Tillman"
+                }
+            ],
+            "greeting": "Hello, Ina England! You have 5 unread messages.",
+            "favoriteFruit": "banana"
+        },
+        {
+            "_id": "66a41543a52cdfd817472264",
+            "index": 6,
+            "guid": "8bbaad74-5cd4-48a1-af3f-35ca3be542c1",
+            "isActive": true,
+            "balance": "$1,408.43",
+            "picture": "http://placehold.it/32x32",
+            "age": 32,
+            "eyeColor": "blue",
+            "name": "Wilder Morris",
+            "gender": "male",
+            "company": "MAKINGWAY",
+            "email": "wildermorris@makingway.com",
+            "phone": "+1 (924) 460-2695",
+            "address": "756 Visitation Place, Bowie, Arkansas, 6454",
+            "about": "Lorem velit officia ad ex reprehenderit proident mollit esse aliquip ut dolor aliquip qui duis. Do officia enim occaecat sit consectetur laborum. Laboris eiusmod mollit exercitation consectetur ullamco consectetur excepteur sit consequat. Aliqua do velit Lorem aute. Nostrud mollit nisi incididunt nisi culpa pariatur aute est dolore do. Nulla non consectetur commodo excepteur aliqua ad incididunt aliquip incididunt nulla consectetur laboris enim est. Ad elit veniam laborum non occaecat eiusmod.\r\n",
+            "registered": "2017-09-15T01:12:10 -03:00",
+            "latitude": 5.521092,
+            "longitude": -41.974781,
+            "tags": [
+                "ex",
+                "quis",
+                "nostrud",
+                "ad",
+                "reprehenderit",
+                "ullamco",
+                "esse"
+            ],
+            "friends": [
+                {
+                    "id": 0,
+                    "name": "Brock Erickson"
+                },
+                {
+                    "id": 1,
+                    "name": "Reese Acosta"
+                },
+                {
+                    "id": 2,
+                    "name": "Lopez Macias"
+                }
+            ],
+            "greeting": "Hello, Wilder Morris! You have 9 unread messages.",
+            "favoriteFruit": "strawberry"
+        }
+    ]
+]
